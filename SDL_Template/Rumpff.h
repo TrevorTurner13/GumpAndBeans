afdd7c270d47fc39476070236cf0c087d966c2df
TrevorTurner13 <118460902+TrevorTurner13@users.noreply.github.com>
2023-07-07 07:38:34 -0700

    added move time max to rumpff

--- SDL_Template/Rumpff.h
+++ SDL_Template/Rumpff.h
@@ -20,6 +20,7 @@ private:
 
 	float mMoveSpeed;
 	float mMoveTime;
+	float mMoveTimeMax;
 	int mMoveDirection;
 
 	Vector2 mMoveBounds;
@@ -32,6 +33,7 @@ public:
 	~Rumpff();
 
 	void RumpffMovement();
+	void SetMoveTimeMax(float change);
 
 	void Update() override;
 	void Render() override;
